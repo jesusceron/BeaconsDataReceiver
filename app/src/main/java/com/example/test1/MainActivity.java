c5cde2597ee1ae18782d32be9941fafce0ba84aa
jesusceron <jdceronbravo@gmail.com>
2019-12-30 10:25:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final char[] HEX = "0123456789ABCDEF".toCharArray();
+    // The Eddystone Service UUID, 0xFEAA.
+    private static final ParcelUuid ESTIMOTE_SERVICE_UUID = ParcelUuid.fromString("0000FE9A-0000-1000-8000-00805F9B34FB");
+    private static final ParcelUuid EDDYSTONE_SERVICE_UUID = ParcelUuid.fromString("0000FEAA-0000-1000-8000-00805F9B34FB");
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 2;
+    private static final ScanSettings SCAN_SETTINGS =
+            new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).setReportDelay(0)
+                    .build();
+
+
+    private List<ScanFilter> scanFilters;
+    public BluetoothManager BTmanager;
+    public BluetoothAdapter BTadapter;
+    public BluetoothLeScanner BTscanner;
+    public ScanCallback scanCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        scanFilters = new ArrayList<>();
+        scanFilters.add(new ScanFilter.Builder().setServiceUuid(ESTIMOTE_SERVICE_UUID).build());
+        scanFilters.add(new ScanFilter.Builder().setServiceUuid(EDDYSTONE_SERVICE_UUID).build());
+
+        scanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                ScanRecord scanRecord = result.getScanRecord();
+
+                if (scanRecord == null) {
+                    return;
+                }
+
+
+                //Log.d("OJO", toHexString(scanRecord.getBytes())); mirar advertisment completo
+
+
+                byte[] serviceData1 = result.getScanRecord().getServiceData(ESTIMOTE_SERVICE_UUID);
+
+                if (serviceData1!=null){
+                    final int rssi = result.getRssi();
+                    //validateServiceData(deviceAddress, serviceData);
+                    Log.d("Estimote", toHexString(serviceData1));
+                }
+
+                byte[] serviceData2 = result.getScanRecord().getServiceData(ESTIMOTE_SERVICE_UUID);
+
+                if (serviceData2!=null){
+                    final int rssi = result.getRssi();
+                    //validateServiceData(deviceAddress, serviceData);
+                    Log.d("Eddystone", toHexString(serviceData2));
+                }
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+                switch (errorCode) {
+                    case SCAN_FAILED_ALREADY_STARTED:
+                        logErrorAndShowToast("SCAN_FAILED_ALREADY_STARTED");
+                        break;
+                    case SCAN_FAILED_APPLICATION_REGISTRATION_FAILED:
+                        logErrorAndShowToast("SCAN_FAILED_APPLICATION_REGISTRATION_FAILED");
+                        break;
+                    case SCAN_FAILED_FEATURE_UNSUPPORTED:
+                        logErrorAndShowToast("SCAN_FAILED_FEATURE_UNSUPPORTED");
+                        break;
+                    case SCAN_FAILED_INTERNAL_ERROR:
+                        logErrorAndShowToast("SCAN_FAILED_INTERNAL_ERROR");
+                        break;
+                    default:
+                        logErrorAndShowToast("Scan failed, unknown error code");
+                        break;
+                }
+            }
+        };
+
+
+    }
+
+    public void onResume(){
+        super.onResume();
+        if (BTscanner != null) {
+            BTscanner.startScan(scanFilters,SCAN_SETTINGS,scanCallback);
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+        if (BTscanner != null) {
+            BTscanner.stopScan(scanCallback);
+        }
+    }
+
+
+    // Attempts to create the scanner.
+    private void init() {
+        // New Android M+ permission check requirement.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs coarse location access");
+                builder.setMessage("Please grant coarse location access so this app can scan for beacons");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+        BTmanager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BTadapter = BTmanager.getAdapter();
+        if (BTadapter == null) {
+            showFinishingAlertDialog("Bluetooth Error", "Bluetooth not detected on device");
+        } else if (!BTadapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+        } else {
+            BTscanner = BTadapter.getBluetoothLeScanner();
+        }
+    }
+
+    // Pops an AlertDialog that quits the app on OK.
+    private void showFinishingAlertDialog(String title, String message) {
+        new AlertDialog.Builder(this).setTitle(title).setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).show();
+    }
+
+    static String toHexString(byte[] bytes) {
+        if (bytes.length == 0) {
+            return "";
+        }
+        char[] chars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int c = bytes[i] & 0xFF;
+            chars[i * 2] = HEX[c >>> 4];
+            chars[i * 2 + 1] = HEX[c & 0x0F];
+        }
+        return new String(chars).toLowerCase();
+    }
+
+    private void logErrorAndShowToast(String message) {
+        Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.e("error", message);
+    }
+}
